b71baf66d1547a1a2a10f9c8fa33f6ee9d24124c
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2023-12-22 10:10:49 +0700

    Add project files.

---
+++ ASP.NET_Core_MVC_(.NET 6)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Core_MVC__.NET_6_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

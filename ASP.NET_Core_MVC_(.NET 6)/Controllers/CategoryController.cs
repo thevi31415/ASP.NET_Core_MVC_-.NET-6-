14cf8db4b34757b37d2f09e0a5b3b80543560559
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2023-12-22 16:54:57 +0700

    Lấy list Category

---
+++ ASP.NET_Core_MVC_(.NET 6)/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using ASP.NET_Core_MVC__.NET_6_.Data;
+using ASP.NET_Core_MVC__.NET_6_.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_MVC__.NET_6_.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategrogyList = _db.Categories.ToList();
+
+            return View(objCategrogyList);
+        }
+    }
+}
